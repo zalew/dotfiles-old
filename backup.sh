9620337f5034f2022e2cf6f2d71bce86cbe1cec6
Jakub Zalewski <zalew7@gmail.com>
2013-05-26 03:14:32 +0200

    2013-05-26 backup

--- backup.sh
+++ backup.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 FILES=".config/xfce4
-.config/Terminal
+.config/openbox
+.config/crunchbang
+.config/gtk-3.0
+.config/tint2
+.config/dmenu
+.config/terminator
 .config/Thunar
 .config/darktable/darktablerc
+.config/transmission
 .config/user-dirs.dirs
-.config/autostart/conky.desktop
 .conky
 .conkyrc
 .fonts.conf
 .fonts
+.icons
 .bash_aliases
 .bashrc
 .zshrc
 .gitconfig
 .git-completion.sh
 .pep8
-lshw.txt"
-/etc/apt/sources.list
+lshw.txt
+/etc/apt/sources.list"
 
 #copy to backup dir
 
@@ -32,7 +38,7 @@ NOW=$(date +"%Y-%m-%d")
 cd ~/dotfiles
 git add -A
 git commit -m "$NOW backup"
-git push -u origin master
+git push -u origin crunchbang-waldorf
 
 
 exit 0
